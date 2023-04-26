9b399f7aa4e54433f02a66793efbd646b2b94901
Anna Escolaro <annaescolaro@hotmail.com>
2023-04-26 16:24:29 -0300

    requisito 3 e corrige 1

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
